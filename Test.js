5d2c6848af283432954610b021db57d8e3bf098e
Yasin Sisto <yasin.sisto3038@gmail.com>
2024-01-13 12:30:05 -0600

    changed test.js a lil bit

--- Test.js
+++ Test.js
@@ -2,13 +2,13 @@
 
 var base = 10;
 
-var vibes = prompt("What is it giving on a scale of 1 to 10?");
+var answer = prompt("What is it giving on a scale of 1 to 10?");
 
-var more_vibes = parseInt(vibes)
+var vibes = parseInt(answer)
 
-vibes = base + more_vibes;
+var Total_vibes = base + vibes;
 
-if (vibes==20){
+if (Total_vibes==20){
     // Gen Z humor
     alert("OMG PURRRR. GO OFF SIS")
 
@@ -18,4 +18,7 @@ if (vibes==20){
 
 }
 
-console.log("The \" vibes \" should've been 10 and you answered:" + vibes)
+console.log("Vibes: " + vibes + " Total Vibes: " + Total_vibes + " Wanted Vibes: 20"
+
+
+)
